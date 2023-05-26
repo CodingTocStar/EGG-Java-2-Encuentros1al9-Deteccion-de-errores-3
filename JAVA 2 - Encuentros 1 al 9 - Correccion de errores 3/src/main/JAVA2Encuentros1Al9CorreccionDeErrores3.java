bdf1704c3500c3b52d68f946eef823f768efb40e
gino <nahuel.dmz@outlook.com>
2023-05-26 02:03:55 -0300

    completo

---
+++ JAVA 2 - Encuentros 1 al 9 - Correccion de errores 3/src/main/JAVA2Encuentros1Al9CorreccionDeErrores3.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author MiriamNahuel
+ */
+public class JAVA2Encuentros1Al9CorreccionDeErrores3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        HashMap<Integer, String> personas = new HashMap();
+        String n1 = "Albus";
+        String n2 = "Severus";
+        personas.put(1, n1);
+        personas.put(2, n2);
+    }
+
+}
